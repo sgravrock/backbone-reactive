597438fef263b5f09e072996a169410c303fa593
Steve Gravrock <sdg@panix.com>
2014-02-07 23:36:19 -0800

    Recompute properties when dependencies change

--- backbone.reactive.js
+++ backbone.reactive.js
@@ -8,14 +8,27 @@ Backbone.ReactiveModel = function (attributes, options) {
 	}
 
 	Backbone.Model.call(this, attributes, options);
+	this._reactiveSpec = reactive;
 
 	_.chain(reactive).keys().each(function (propName) {
-		// Get the value for each dependency
+		// Compute the initial for the dependency
 		// TODO: allow dependencies to be on different model objects
-		var depValues = _.map(reactive[propName].deps, that.get.bind(that));
-		var value = reactive[propName].compute.apply(null, depValues);
-		that.set(propName, value);
+		that._reactiveRecompute(propName);
+
+		// When a dependency changes, recompute all properties that depend on it.
+		_.each(reactive[propName].deps, function (depName) {
+			that.on("change:" + depName, function () {
+				that._reactiveRecompute(propName);
+			});
+		});
 	});
 };
 
-_.extend(Backbone.ReactiveModel.prototype, Backbone.Model.prototype);
+_.extend(Backbone.ReactiveModel.prototype, Backbone.Model.prototype, {
+	_reactiveRecompute: function (propName) {
+		var depValues = _.map(this._reactiveSpec[propName].deps,
+				this.get.bind(this));
+		var value = this._reactiveSpec[propName].compute.apply(null, depValues);
+		this.set(propName, value);
+	}
+});
--- tests.js
+++ tests.js
@@ -1,13 +1,28 @@
 describe("Reactive Backbone properties", function () {
-	it("should have the value returned by the compute function", function () {
-		var target = new Backbone.ReactiveModel({
+	var target;
+
+	beforeEach(function () {
+		target = new Backbone.ReactiveModel({
 			a: 1,
 			b: 2,
 			reactive: {
-				"c": { deps: ["a", "b"], compute: function (a, b) { return a + b; } }
+				"c": {
+					deps: ["a", "b"],
+					compute: function (a, b) { return a + b; }
+				}
 			}
 		});
+	});
 
+	it("should have the value returned by the compute function", function () {
 		expect(target.get("c")).toEqual(3);
+	});
+
+	it("should recompute when any dependency changes", function () {
+		var listener = jasmine.createSpy("change listener");
+		target.on("change:c", listener);
+		target.set("a", 3);
+		expect(target.get("c")).toEqual(5, "Wrong value");
+		expect(listener).toHaveBeenCalledWith(target, 5, {});
 	});
 });
