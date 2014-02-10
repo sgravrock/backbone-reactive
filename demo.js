1b65bc7e329a62d13c52792190716ec4a5fbbbd2
Steve Gravrock <sdg@panix.com>
2014-02-09 17:28:31 -0800

    Updated demo to take advantage of new features

--- demo.js
+++ demo.js
@@ -1,4 +1,44 @@
 (function () {
+	var sum = function () {
+		var i, total = 0;
+
+		for (i = 0; i < arguments.length; i++) {
+			total += arguments[i];
+		}
+
+		return total;
+	};
+
+	var CartItem = Backbone.ReactiveModel.extend({
+		reactive: {
+			totalPrice: {
+				deps: ["unitPrice", "qty"],
+				compute: function (unitPrice, qty) {
+					return unitPrice * qty;
+				}
+			}
+		}
+	});
+
+	var CartItemList = Backbone.Collection.extend({
+		model: CartItem
+	});
+
+	var Cart = Backbone.ReactiveModel.extend({
+		constructor: function () {
+			Backbone.ReactiveModel.call(this, {
+				items: new CartItemList()
+			});
+		},
+		reactive: {
+			grandTotalPrice: {
+				collection: "items",
+				pluck: "totalPrice",
+				compute: sum
+			}
+		}
+	});
+
 	var CartItemView = Marionette.ItemView.extend({
 		template: "#template-cartItemView",
 		tagName: "tr",
@@ -35,42 +75,16 @@
 	var app = new Marionette.Application();
 
 	app.addInitializer(function () {
-		var items = new Backbone.Collection(_.map([
+		var cart = new Cart();
+		cart.get("items").reset([
 			{ name: "Case of dynamite", unitPrice: 60, qty: 1 },
 			{ name: "Anvil", unitPrice: 50, qty: 2 },
-			{ name: "Paint-on tunnel", unitPrice: 20, qty: 1 }
-		], function (spec) {
-			spec.reactive = {
-				totalPrice: {	
-					deps: ["unitPrice", "qty"],
-					compute: function (unitPrice, qty) {
-						return unitPrice * qty;
-					}
-				}
-			};
-			return new Backbone.ReactiveModel(spec);
-		}));
-
-		var cart = new Backbone.ReactiveModel({
-			reactive: {
-				grandTotalPrice: {
-					collection: items,
-					pluck: "totalPrice",
-					compute: function () {
-						var i, sum = 0;
-
-						for (i = 0; i < arguments.length; i++) {
-							sum += arguments[i];
-						}
-
-						return sum;
-					}
-				}
-			}
-		});
+			{ name: "Paint-on tunnel", unitPrice: 20, qty: 1 },
+			{ name: "Earthquake pills", unitPrice: 5, qty: 1 }
+		]);
 		app.root.show(new CartView({
 			model: cart,
-			collection: items
+			collection: cart.get("items")
 		}));
 	});
 
